2845a1a8ef989aae5e7dcd87461a9e8d2ea7e1cd
Ricardo Rolim <ricardorolim27@gmail.com>
2021-01-08 23:37:33 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
+import Footer from './Footer';
 import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Home />
+      <>
+        <Navbar />
+        <Home />
+      </>
     </div>
   );
 }
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,29 +1,33 @@
 import './styles.css';
-import {ReactComponent as HomeImg} from "./home-image.svg";
+import { ReactComponent as HomeImg } from "./home-image.svg";
+import Footer from '../Footer';
 
-function Home(){
-    return(
-        <div className="home-container">
-            <div className="home-content">
-               <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido <br/>
-                        que entregamos <br/>
-                        pra você!!!
-                    </h1>
-                    <h3 className="home-subtitle"> 
-                        Escolha seu pedido que em pouco minutos <br/>
-                        levaremos na sua porta.
-                    </h3>
-                    <a href="orders" className="home-btn-order">
-                        FAZER PEDIDO
-                    </a>
-               </div>
-               <div className="home-image">
-                    <HomeImg />
-               </div>
+function Home() {
+    return (
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido <br />
+                            que entregamos <br />
+                            pra você!!!
+                        </h1>
+                        <h3 className="home-subtitle">
+                            Escolha seu pedido que em pouco minutos <br />
+                            levaremos na sua porta.
+                        </h3>
+                        <a href="orders" className="home-btn-order">
+                            FAZER PEDIDO
+                        </a>
+                    </div>
+                    <div className="home-image">
+                        <HomeImg />
+                    </div>
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
