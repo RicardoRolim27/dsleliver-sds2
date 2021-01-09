5c84b5536951b90fc04450160fe971e7fa46a861
Ricardo Rolim <ricardorolim27@gmail.com>
2021-01-08 22:52:48 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,30 @@
+import './styles.css';
+import {ReactComponent as HomeImg} from "./home-image.svg";
+
+function Home(){
+    return(
+        <div className="home-container">
+            <div className="home-content">
+               <div className="home-actions">
+                    <h1 className="home-title">
+                        Faça seu pedido <br/>
+                        que entregamos <br/>
+                        pra você!!!
+                    </h1>
+                    <h3 className="home-subtitle"> 
+                        Escolha seu pedido que em pouco minutos <br/>
+                        levaremos na sua porta.
+                    </h3>
+                    <a href="orders" className="home-btn-order">
+                        FAZER PEDIDO
+                    </a>
+               </div>
+               <div className="home-image">
+                    <HomeImg />
+               </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
